ea0422a2b395ffd4b9545980c10e1d384beef211
Alix JEUDI--LEMOINE <Alix.JEUDI--LEMOINE@etu.uca.fr>
2023-10-28 23:37:21 +0200

    Throw Exception dans la classe Validation

--- project/src/config/Validation.php
+++ project/src/config/Validation.php
@@ -12,14 +12,16 @@ class Validation
 
     public static function val_form(string &$jeu, string &$difficulty, &$dVueErreur)
     {
-        if (!isset($jeu) || $jeu == '' || !filter_var($jeu, FILTER_VALIDATE_INT)) {
+        if ($jeu == '' || !filter_var($jeu, FILTER_SANITIZE_STRING)) {
             $dVueErreur[] = 'Aucun jeu selectionné';
             $jeu          = 0;
+            throw new \Exception("Erreur jeu");
         }
 
-        if (!isset($difficulty) || $difficulty == '' || !filter_var($difficulty, FILTER_VALIDATE_INT)) {
+        if ($difficulty == '' || !filter_var($difficulty, FILTER_VALIDATE_INT)) {
             $dVueErreur[] = "Aucune difficultée séléctionnée";
             $difficulty   = 0;
+            throw new \Exception("Erreur difficulté");
         }
     }
 }
