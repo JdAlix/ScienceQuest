af0f01f87d80dbe8f157dfb6fb9864d5c48e55fa
victor.soulier <victor.soulier@etu.uca.fr>
2023-11-16 20:22:06 +0100

    Set test db config

--- project/src/config/config.php
+++ project/src/config/config.php
@@ -3,8 +3,8 @@ namespace config;
 
 $config = [
     "rep" => __DIR__.'/../',
-    "db" => ["dsn" => '',
-             "login" => '',
+    "db" => ["dsn" => 'pgsql:host=localhost;port=5432;dbname=sae2a',
+             "login" => 'postgres',
              "mdp" =>''],
     "vues" => ["index" => 'vues/index.php']
 ];
