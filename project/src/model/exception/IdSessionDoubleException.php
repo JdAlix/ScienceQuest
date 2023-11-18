c8b07b1c1051857ef30f3c55895cc3fb790a8d16
victor.soulier <victor.soulier@etu.uca.fr>
2023-11-18 19:02:07 +0100

    ADD : attributs id et idsession

--- project/src/model/exception/IdSessionDoubleException.php
+++ project/src/model/exception/IdSessionDoubleException.php
@@ -4,5 +4,21 @@ namespace model;
 
 class IdSessionDoubleException extends \RuntimeException
 {
+    private int $idJoueur;
+    private string $idSession;
 
+    public function __construct($idJoueur, $idSession)
+    {
+        parent::__construct();
+        $this->idJoueur=$idJoueur;
+        $this->idSession=$idSession;
+    }
+
+    public function getIdJoueur():int{
+        return $this->idJoueur;
+    }
+
+    public function getIdSession(): string{
+        return $this->idSession;
+    }
 }
