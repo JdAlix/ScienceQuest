082030277aef265c9333924d239c3b14337e720f
victor.soulier <victor.soulier@etu.uca.fr>
2023-11-19 18:35:26 +0100

    FIX : suppression char invalides

--- project/src/db.sql
+++ project/src/db.sql
@@ -170,5 +170,5 @@ INSERT INTO Thematique(libelle) VALUES ('Nucléaire'),('Mathématiques');
 
 INSERT INTO Scientifique(nom, prenom, photo, dateNaissance, descriptif, ratioTrouvee, idThematique, idDifficulte, idSexe)
 VALUES
-    ('nom0', 'prenom0', '', CURRENT_DATE, 'desc0', 0, 1, 1, 1),
-    ('nom1', 'prenom1', '', CURRENT_DATE, 'desc1', 0, 2, 2, 2);
+    ('nomToto', 'prenomToto', '', CURRENT_DATE, 'desc', 0, 1, 1, 1),
+    ('nomTiti', 'prenomTiti', '', CURRENT_DATE, 'desc', 0, 2, 2, 2);
