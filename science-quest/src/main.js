b7d8354d45b56d948bb0b7594dfbda7bee2a64db
gwen <a>
2024-02-26 16:22:28 +0100

    enlever commentaire

--- science-quest/src/main.js
+++ science-quest/src/main.js
@@ -16,8 +16,6 @@ const routes = [
 ]
 
 const router = createRouter({
-    // 4. Provide the history implementation to use. We
-    // are using the hash history for simplicity here.
     history: createWebHashHistory(),
     routes, // short for `routes: routes`
 })
