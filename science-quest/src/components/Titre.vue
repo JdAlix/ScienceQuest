8e612665dae7b10da23b264370c1c6e62ea3e41c
gwen <a>
2024-02-26 15:01:29 +0100

    ajout header du site temporaire

---
+++ science-quest/src/components/Titre.vue
@@ -0,0 +1,66 @@
+<script>
+export default{
+    data(){
+        return{
+            nomApp:"ScienceQuest"
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <header>
+        <!-- logo temporaire-->
+        <img alt="Vue logo" src="../assets/logo.svg" />
+        <h1>
+            <a href="/">
+                {{ nomApp }}
+            </a>
+        </h1>
+        <div class="espacement">
+            <!-- espacement pour mettre les boutons a droite-->
+        </div>
+
+        <!-- liste de trucs a cliquer; accueil, jouer, langues?-->
+        <nav>
+            <!-- todo : component pour faire type de bouton (normal, menu déroulant)-->
+            <a>Accueil</a>
+            <a>Jouer</a>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header{
+    width:100%;
+    height: 4em;
+    background-color:gold;
+    display:flex;
+    flex:auto;
+    flex-direction: row;
+}
+
+.espacement{
+    flex:auto
+}
+
+h1{
+    font-size: 2em;
+}
+
+nav{
+    display:flex;
+    flex:0;
+    flex-direction: row;
+}
+
+nav a{
+    background-color: aqua;
+    vertical-align: center;
+    padding:1em;
+    height:100%;
+}
+
+</style>
