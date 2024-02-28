b73afa868b3221e8cf575f132b083e3e539a1a8f
visoulier <victor.soulier@etu.uca.fr>
2024-02-28 03:59:29 +0100

    ajustement nom exception

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/exceptions/DuplicatedIdException.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/exceptions/DuplicatedIdException.java
@@ -1,12 +1,11 @@
 package fr.iut.sciencequest.sae.exceptions;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class DuplicatedEntity extends RuntimeException {
-    public DuplicatedEntity(String message){
-        super(message);
+public class DuplicatedIdException extends RuntimeException{
+    public DuplicatedIdException(){
+        super("duplicated id");
     }
 }
