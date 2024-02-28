9c809a024f03903a498761b51af2b3cb338f7976
visoulier <victor.soulier@etu.uca.fr>
2024-02-28 03:58:06 +0100

    suppression import inutilisé

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/entities/Thematique.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/entities/Thematique.java
@@ -1,12 +1,13 @@
 package fr.iut.sciencequest.sae.entities;
 
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.UniqueElements;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,7 +16,6 @@ import org.hibernate.validator.constraints.UniqueElements;
 @Table(name="thematique")
 public class Thematique {
     @Id
-    @NotNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
