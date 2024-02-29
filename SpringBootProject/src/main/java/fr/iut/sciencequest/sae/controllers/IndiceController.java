8826093d9157bfedc7f1d9bb69b1a799c8a8270b
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-02-29 18:08:12 +0100

    Modification constructeur

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/IndiceController.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/IndiceController.java
@@ -2,18 +2,16 @@ package fr.iut.sciencequest.sae.controllers;
 
 import fr.iut.sciencequest.sae.entities.indice.Indice;
 import fr.iut.sciencequest.sae.services.IndiceService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/indices")
 public class IndiceController extends Controller {
     private final IndiceService indiceService;
-
-    public IndiceController(IndiceService indiceService) {
-        this.indiceService = indiceService;
-    }
 
     @PatchMapping(value="/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
