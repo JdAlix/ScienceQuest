fa70b65435d39384d8f7d4f9352520ab1d37f154
gwen <a>
2024-02-29 18:19:28 +0100

    fix tableau

--- science-quest/src/components/admin/VoirScientifiques.vue
+++ science-quest/src/components/admin/VoirScientifiques.vue
@@ -7,22 +7,25 @@ export default{
     data() {
         return {
             //données obtenues par l'api
-            scientifiques: [
-            ],
+            scientifiques: [],
             page:0,
         };
     },
     mounted(){
-        this.getScientifiques(0)
+        //TODO faire route pour prendre la page a partir de l'URL
+        this.getScientifiques(this.page)
     },
     methods:{
         getScientifiques(page){
+            //enlever les anciens du tableau
+            this.scientifiques.splice(0)
+            //TODO : ajouter un delai si jamais la requete est trop rapide pour VueJS
             //appeler l'API
             fetch(`${REST_API}/scientifiques?page=${page}`).then(response=>{
                 response.json().then(json=>{
+                    const oldLength=this.scientifiques.length
                     //prendre le scientifique de la requete
-                    this.scientifiques=json._embedded
-
+                    this.scientifiques.push(...json._embedded)
                 })
             })
         }
@@ -52,5 +55,6 @@ export default{
     ></LigneScientifique>
 </tbody>
     </table>
+    <button @click="this.getScientifiques(++this.page)">Next</button>
     <!-- TODO : pagination -->
 </template>
