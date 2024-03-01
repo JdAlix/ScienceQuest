bc9aa85119edfc74321219df69c6723e95513902
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-01 13:05:17 +0100

    Ajout recherche en DB des questions par scientifique (query à la main car réussi à faire en auto)

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/repositories/QuestionRepository.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/repositories/QuestionRepository.java
@@ -1,13 +1,15 @@
 package fr.iut.sciencequest.sae.repositories;
 
 import fr.iut.sciencequest.sae.entities.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
-
+    @Query("SELECT q FROM Question q JOIN Reponse r ON q.id = r.id WHERE r.scientifique.id = :scientifiqueId")
+    Page<Question> findAllQuestionsByScientifiqueId(Pageable page, @Param("scientifiqueId") Integer scientifiqueId);
 }
