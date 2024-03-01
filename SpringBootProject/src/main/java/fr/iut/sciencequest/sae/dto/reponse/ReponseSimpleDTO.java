431288aac7397304eaff285586075821a9cf0d11
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-01 13:06:38 +0100

    Ajout ReponseSimpleDTO

---
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/dto/reponse/ReponseSimpleDTO.java
@@ -0,0 +1,22 @@
+package fr.iut.sciencequest.sae.dto.reponse;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ReponseSimpleDTO extends RepresentationModel<ReponseSimpleDTO> {
+    @NotNull
+    private Integer id;
+
+    @NotNull
+    @NotBlank
+    private String reponse;
+}
