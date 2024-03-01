c2f6ac474d3474c6e207cccf34e4567081e663ad
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-01 13:06:58 +0100

    Correction faute Exception thematique

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/exceptions/notFound/ThematiqueNotFoundException.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/exceptions/notFound/ThematiqueNotFoundException.java
@@ -2,6 +2,6 @@ package fr.iut.sciencequest.sae.exceptions.notFound;
 
 public class ThematiqueNotFoundException extends EntityNotFoundException{
     public ThematiqueNotFoundException(int id) {
-        super("Thématique", id);
+        super("Thematique", id);
     }
 }
