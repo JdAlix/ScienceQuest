d903bf5d6d7274023de1f0788f1e09af2f4615f8
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-06 15:20:40 +0100

    Pour plus tard si ajout de spring security

---
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/SecurityConfig.java
@@ -0,0 +1,20 @@
+package fr.iut.sciencequest.sae;
+
+/*
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}*/
