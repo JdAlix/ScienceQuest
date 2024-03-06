d8a920bf0845239087b535bb09528f936a295541
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-06 15:21:28 +0100

    Suppression de partie dans UtilisateurDTO (partie est dans Joueur)

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/dto/utilisateur/UtilisateurDTO.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/dto/utilisateur/UtilisateurDTO.java
@@ -1,7 +1,6 @@
 package fr.iut.sciencequest.sae.dto.utilisateur;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import fr.iut.sciencequest.sae.dto.partie.PartieDTO;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -26,5 +25,4 @@ public class UtilisateurDTO extends RepresentationModel<UtilisateurDTO> {
     @NotBlank
     private String pseudo;
 
-    private PartieDTO partie;
 }
