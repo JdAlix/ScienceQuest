d6203b0dbdfeb464b52f6522c8dca91cb63423a6
visoulier <victor.soulier@etu.uca.fr>
2024-03-12 11:48:49 +0100

    fix missing column

--- android/app/src/main/java/fr/iut/sciencequest/view/HomeScreen.kt
+++ android/app/src/main/java/fr/iut/sciencequest/view/HomeScreen.kt
@@ -1,12 +1,20 @@
 package fr.iut.sciencequest.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(goToHome: () -> Unit, goToAccount: () -> Unit) {
-    TopBar(goToAccount, goToHome)
-    MainContent()
+    Column (modifier = Modifier.fillMaxWidth()){
+        TopBar(goToAccount, goToHome)
+        MainContent()
+    }
 }
 
 @Preview
