a069a87d5edcd4cf7ba06d2294f70ca43a76ed25
visoulier <victor.soulier@etu.uca.fr>
2024-03-12 11:44:13 +0100

    ajout preview

--- android/app/src/main/java/fr/iut/sciencequest/view/AccountScreen.kt
+++ android/app/src/main/java/fr/iut/sciencequest/view/AccountScreen.kt
@@ -5,11 +5,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun AccountScreen(goToAccount: () -> Unit, goToHome: () -> Unit) {
     Column(modifier = Modifier.fillMaxWidth()) {
         TopBar(goToAccount, goToHome)
         Text(text = "Account")
+    }
+}
+
+@Preview
+@Composable
+fun AccountScreenPreview(){
+    AccountScreen(goToAccount = {}) {
+        
     }
 }
