d4bd57b378665cad1b0235b4cb34fe0d1f098dca
Alix JEUDI--LEMOINE <Alix.JEUDI--LEMOINE@etu.uca.fr>
2024-03-13 09:28:02 +0100

    Ajout padding pour éviter que le contenu soit collé à la barre de navigation

--- android/app/src/main/java/fr/iut/sciencequest/view/AccountScreen.kt
+++ android/app/src/main/java/fr/iut/sciencequest/view/AccountScreen.kt
@@ -2,18 +2,20 @@ package fr.iut.sciencequest.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.iut.sciencequest.R
 
 @Composable
 fun AccountScreen(goToAccount: () -> Unit, goToHome: () -> Unit) {
     Column(modifier = Modifier.fillMaxWidth()) {
         TopBar(goToAccount, goToHome)
-        Text(text = stringResource(id = R.string.Compte))
+        Text(text = stringResource(id = R.string.Compte), modifier = Modifier.padding(top=10.dp))
     }
 }
 
