e237947f5c8618b8e69b9d06209c81b930cd692b
Alix JEUDI--LEMOINE <Alix.JEUDI--LEMOINE@etu.uca.fr>
2024-03-13 09:29:17 +0100

    Ajout Toast sur les boutons qui affiche 'Coming Soon' + replacement des Dp(xf) par x.dp (pas beau du tout)

--- android/app/src/main/java/fr/iut/sciencequest/view/HomeScreen.kt
+++ android/app/src/main/java/fr/iut/sciencequest/view/HomeScreen.kt
@@ -1,18 +1,18 @@
 package fr.iut.sciencequest.view
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -31,16 +31,18 @@ fun HomeScreenPreview() {
 
 @Composable
 fun MainContent() {
-    Column (modifier= Modifier.fillMaxWidth().padding(Dp(30f)), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(
-        Dp(15f)
+    val context = LocalContext.current;
+    val comingSoon = Toast.makeText(context, "Coming soon", Toast.LENGTH_SHORT);
+
+    Column (modifier = Modifier.fillMaxWidth().padding(30.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(15.dp
     )){
-        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+        Button(onClick = { comingSoon.show() }, Modifier.fillMaxWidth()) {
             Text(text = "Pendu", fontSize = 13.sp)
         }
-        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+        Button(onClick = { comingSoon.show() }, Modifier.fillMaxWidth()) {
             Text(text = "Kahoot", fontSize = 13.sp)
         }
-        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+        Button(onClick = { comingSoon.show() }, Modifier.fillMaxWidth()) {
             Text(text = "Qui est ce ?", fontSize = 13.sp)
         }
     }
