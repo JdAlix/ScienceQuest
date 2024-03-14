0de04c58b16323089456c9b143436ec72c830aa5
gwenael.planchon <gwenael.planchon@etu.uca.fr>
2024-03-14 14:29:23 +0100

    rendre ajout.vue versatile

--- science-quest/src/components/admin/gestion/Ajout.vue
+++ science-quest/src/components/admin/gestion/Ajout.vue
@@ -2,27 +2,26 @@
 import { REST_API } from '@/assets/const';
 
 export default{
+    props:["donnee", "endpoint"], //format : {"nomColonne":"typeDonnee", ...} => {"nom":"text", "desc":"text", ...}
     methods:{
         envoyerDonnees: function(event){
             const donnees=new FormData(formajouter)
 
-            //todo mettre lien dans const
             //envoyer le form en JSON
-            fetch(REST_API+"/thematiques", {method:"POST", body:JSON.stringify(Object.fromEntries(donnees)), headers: {"Content-Type": "application/json"}})
+            fetch("localhost"+"/"+this.endpoint, {method:"POST", body:JSON.stringify(Object.fromEntries(donnees)), headers: {"Content-Type": "application/json"}})
             //sans le JSON.stringify et Object.fromEntries ca fait une requete en Content-Disposition
         }
     }
 }
-
-
-//TODO: verifier si on est admin quand on entre dans la partie admin
 </script>
 
 <template>
 <form id="formajouter" @submit.prevent>
     <div>
-        <label for="libelle">Libelle</label>
-        <input class="form-control" type="text" id="libelle" name="libelle"/>
+        <fieldset v-for="nomColonne in Object.keys(donnee??{})">
+            <label :for="nomColonne+'_temp_add_form'">{{nomColonne}}</label>
+            <input class="form-control" :type="donnee[nomColonne]" :id="nomColonne+'_temp_add_form'" :aria-label="nomColonne"/>
+        </fieldset>
     </div>
     
     <button v-on:click="envoyerDonnees" class="btn btn-primary">Ajouter</button>
