e9116836626b1bf2853380a836c39760e6790986
Renaud BEURET <renaud.beuret@etu.uca.fr>
2024-03-19 17:25:15 +0100

    [ADD] Service de requêtes

---
+++ android/app/src/main/java/fr/iut/sciencequest/model/RequestService.kt
@@ -0,0 +1,31 @@
+package fr.iut.sciencequest.model
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import fr.iut.sciencequest.model.dto.ScientifiqueDTO
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+// a remplir
+private const val API_BASE_URL = ""
+
+val httpClient = OkHttpClient()
+
+interface RequestService {
+    @GET("scientifiques/{index}/{count}")
+    fun getScientifiques(@Path("index") index: Int, @Path("count") count: Int): Call<List<ScientifiqueDTO>>
+
+    @GET("scientifiques/{id}")
+    fun getScientifique(@Path("id") id: Int)
+}
+
+fun createRequestService(): Retrofit =
+    Retrofit.Builder()
+        .baseUrl(API_BASE_URL)
+        .addConverterFactory(Json {  ignoreUnknownKeys = true }.asConverterFactory(MediaType.get("application/json")))
+        .client(httpClient)
+        .build()
