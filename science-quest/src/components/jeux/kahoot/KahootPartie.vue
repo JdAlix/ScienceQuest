eab10e835971988c06a77a5b28f1fba1912ae21a
gwenael.planchon <gwenael.planchon@etu.uca.fr>
2024-03-19 13:23:53 +0100

    fix

--- science-quest/src/components/jeux/kahoot/KahootPartie.vue
+++ science-quest/src/components/jeux/kahoot/KahootPartie.vue
@@ -31,7 +31,7 @@ export default {
 				this.question=response.question
 				this.reponses=response.reponses
 				this.dateFinDeLaQuestion=response.tempsLimite
-				if(dateFinDeLaQuestion!=-1){
+				if(this.dateFinDeLaQuestion!=-1){
 					//executer la fonction en boucle jusqu'a ce que la partie se termine
 					window.setTimeout(this.obtenirQuestion,(this.dateFinDeLaQuestion+100)-Date.now())
 				}
