63f60b34b0fa5c0f166eca6ea12dc15829195d81
visoulier <victor.soulier@etu.uca.fr>
2024-03-20 09:32:37 +0100

    extensible dto questionwithsimplereponse

--- android/app/src/main/java/fr/iut/sciencequest/model/dto/question/QuestionWithSimpleResponseDTO.kt
+++ android/app/src/main/java/fr/iut/sciencequest/model/dto/question/QuestionWithSimpleResponseDTO.kt
@@ -1,11 +1,10 @@
 package fr.iut.sciencequest.model.dto.question
 
-import fr.iut.sciencequest.model.dto.reponse.ReponseDTO
 import fr.iut.sciencequest.model.dto.reponse.ReponseSimpleDTO
 import kotlinx.serialization.Serializable
 
 @Serializable
-class QuestionWithSimpleResponseDTO(
+open class QuestionWithSimpleResponseDTO(
     val id: Int,
     val question: String,
     val reponses: List<ReponseSimpleDTO>
