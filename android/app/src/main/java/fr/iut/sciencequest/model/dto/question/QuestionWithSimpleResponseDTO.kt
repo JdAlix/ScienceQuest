cad113168ff025388265c10b64b99ce8fe577bb5
visoulier <victor.soulier@etu.uca.fr>
2024-03-20 09:00:23 +0100

    fix constructor

--- android/app/src/main/java/fr/iut/sciencequest/model/dto/question/QuestionWithSimpleResponseDTO.kt
+++ android/app/src/main/java/fr/iut/sciencequest/model/dto/question/QuestionWithSimpleResponseDTO.kt
@@ -1,11 +1,12 @@
 package fr.iut.sciencequest.model.dto.question
 
 import fr.iut.sciencequest.model.dto.reponse.ReponseDTO
+import fr.iut.sciencequest.model.dto.reponse.ReponseSimpleDTO
 import kotlinx.serialization.Serializable
 
 @Serializable
 class QuestionWithSimpleResponseDTO(
     val id: Int,
     val question: String,
-    val reponses: List<ReponseDTO>
-){}
+    val reponses: List<ReponseSimpleDTO>
+)
