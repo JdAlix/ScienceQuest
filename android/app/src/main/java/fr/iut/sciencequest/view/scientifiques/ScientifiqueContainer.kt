5abf2a8bbb578118dfa4acb40752cbb1a69808e9
Renaud BEURET <renaud.beuret@etu.uca.fr>
2024-03-21 09:55:29 +0100

    [ADD] Border pour container de scientifique

--- android/app/src/main/java/fr/iut/sciencequest/view/scientifiques/ScientifiqueContainer.kt
+++ android/app/src/main/java/fr/iut/sciencequest/view/scientifiques/ScientifiqueContainer.kt
@@ -1,13 +1,17 @@
 package fr.iut.sciencequest.view.scientifiques
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import fr.iut.sciencequest.model.dto.extensions.ToModel
 import fr.iut.sciencequest.model.metier.Scientifique
 import fr.iut.sciencequest.stub.StubScientifique1
@@ -15,7 +19,9 @@ import fr.iut.sciencequest.stub.StubScientifique1
 @Preview
 @Composable
 fun scientifiqueContainer(scientifique: Scientifique = StubScientifique1.ToModel()) {
-    Row {
+    Row (
+        modifier = Modifier.border(4.dp, Color.Blue)
+    ){
         Column {
             Text(scientifique.nom, modifier = Modifier.padding(Dp(3f)))
             Text(scientifique.prenom, modifier = Modifier.padding(Dp(3f)))
