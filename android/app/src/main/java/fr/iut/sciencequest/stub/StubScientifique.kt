ce7675f5aea17f4af60bb7b8595bdc9139c36b66
Renaud BEURET <renaud.beuret@etu.uca.fr>
2024-03-21 08:22:10 +0100

    [ADD] Stub liste scientifique

--- android/app/src/main/java/fr/iut/sciencequest/stub/StubScientifique.kt
+++ android/app/src/main/java/fr/iut/sciencequest/stub/StubScientifique.kt
@@ -1,6 +1,7 @@
 package fr.iut.sciencequest.stub
 
 import fr.iut.sciencequest.model.dto.ScientifiqueDTO
+import fr.iut.sciencequest.model.metier.Scientifique
 
 object StubScientifique1: ScientifiqueDTO (
     id = 1,
@@ -13,4 +14,22 @@ object StubScientifique1: ScientifiqueDTO (
     thematiqueId = 1,
     ratioTrouve = 0f
 )
-{}
+
+object StubScientifique2: ScientifiqueDTO (
+    id = 2,
+    descriptif = "autre description",
+    nom = "Jean2",
+    prenom = "Jean2",
+    photo = "photo2",
+    sexe = 'F',
+    difficulteId = 1,
+    thematiqueId = 1,
+    ratioTrouve = 0f
+)
+
+fun getScientifiqueListeStub(): List<ScientifiqueDTO>{
+    val liste = ArrayList<ScientifiqueDTO>()
+    liste.add(StubScientifique2)
+    liste.add(StubScientifique1)
+    return liste
+}
