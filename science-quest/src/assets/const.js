b2787a7db991e8fd8ccf60ed47c517a84120a0ba
gwenael.planchon <gwenael.planchon@etu.uca.fr>
2024-03-22 11:12:19 +0100

    changer en nombre

--- science-quest/src/assets/const.js
+++ science-quest/src/assets/const.js
@@ -5,4 +5,4 @@ export const REST_API="https://sae-java.alix-jdlm.fr/api/v1"
 export const ALGO_HASH_MDP="SHA-256"
 
 
-export const KAHOOT_NB_APRES_LA_VIRGULE_COMPTE_A_REBOURS="2"
+export const KAHOOT_NB_APRES_LA_VIRGULE_COMPTE_A_REBOURS=2
