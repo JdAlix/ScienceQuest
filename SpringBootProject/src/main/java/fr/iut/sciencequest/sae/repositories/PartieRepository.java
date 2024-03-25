d459563cce5f2835d042a6c7d91980bc2dc80e33
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-25 18:37:02 +0100

    Ajout getPartieByCodeInvitation pour rejoindre par invite

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/repositories/PartieRepository.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/repositories/PartieRepository.java
@@ -4,5 +4,9 @@ import fr.iut.sciencequest.sae.entities.Partie;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface PartieRepository extends CrudRepository<Partie, Integer> {}
+public interface PartieRepository extends CrudRepository<Partie, Integer> {
+    Optional<Partie> getPartieByCodeInvitation(String codeInvitation);
+}
