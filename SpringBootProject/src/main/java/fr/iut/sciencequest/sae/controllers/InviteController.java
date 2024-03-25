4bf5e2276fec7238f684a1d398743135b13e5186
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-25 19:40:15 +0100

    Ajout controller Invite

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/InviteController.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/InviteController.java
@@ -1,2 +1,21 @@
-package fr.iut.sciencequest.sae.controllers;public class InviteController {
+package fr.iut.sciencequest.sae.controllers;
+
+import fr.iut.sciencequest.sae.controllers.request.InviteRequest;
+import fr.iut.sciencequest.sae.services.InviteService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/invite")
+public class InviteController {
+    private final InviteService inviteService;
+
+    @GetMapping(value = "/{invite}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HashMap<String, Object> getParty(@PathVariable String invite, @RequestBody InviteRequest inviteRequest) {
+        return this.inviteService.findByInvite(invite, inviteRequest.getPseudo());
+    }
 }
