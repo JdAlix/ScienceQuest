10965995e03d35ce40110bdd44f93cdc7dce67f1
Alix JEUDI--LEMOINE <alix.jeudi--lemoine@etu.uca.fr>
2024-03-25 18:35:05 +0100

    Création model pour Request create party

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/request/PartieRequest.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/request/PartieRequest.java
@@ -1,2 +1,13 @@
-package fr.iut.sciencequest.sae.controllers.request;public class PartieRequest {
+package fr.iut.sciencequest.sae.controllers.request;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PartieRequest {
+    private Integer idJeu;
+    private String pseudo;
+    private List<Integer> thematiques;
+    private Integer idDifficulte;
 }
