b8982b3e8e6eb6317394dde39ef2b985aabf20c5
visoulier <victor.soulier@etu.uca.fr>
2024-03-27 21:30:39 +0100

    fix champs obligatoires

--- SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/request/PartieRequest.java
+++ SpringBootProject/src/main/java/fr/iut/sciencequest/sae/controllers/request/PartieRequest.java
@@ -1,13 +1,17 @@
 package fr.iut.sciencequest.sae.controllers.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class PartieRequest {
+    @NotNull
     private Integer idJeu;
+    @NotNull
     private int idJoueur;
+
     private List<Integer> thematiques;
     private Integer idDifficulte;
 }
