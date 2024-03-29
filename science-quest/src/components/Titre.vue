e910f0db60dd6808bf865d94815a712cfd48f340
gwenael.planchon <gwenael.planchon@etu.uca.fr>
2024-03-29 10:57:14 +0100

    ajouter bouton creer une partie

--- science-quest/src/components/Titre.vue
+++ science-quest/src/components/Titre.vue
@@ -35,6 +35,9 @@ export default{
         <li class="nav-item">
           <router-link class="nav-link" to="/">Accueil</router-link>
         </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/partie">Créer une partie</router-link>
+        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Jeux
