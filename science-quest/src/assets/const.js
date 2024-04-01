7ce4c629b74e62a281b1b44f05f5994fcc3f2138
Gwenael PLANCHON <gwenael.planchon@etu.uca.fr>
2024-04-01 17:18:28 +0200

    changer l'url

--- science-quest/src/assets/const.js
+++ science-quest/src/assets/const.js
@@ -1,5 +1,5 @@
 export const NOM_APP="ScienceQuest"
 
-export const REST_API="https://sae-java.alix-jdlm.fr/api/v1"
+export const REST_API="https://codefirst.iut.uca.fr/containers/tombiard-api/api/v1"
 
 export const KAHOOT_NB_APRES_LA_VIRGULE_COMPTE_A_REBOURS=2
