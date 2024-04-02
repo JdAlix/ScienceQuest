66decf048621d748f1f3b10ccba2a8406cb03d42
Renaud BEURET <renaud.beuret@etu.uca.fr>
2024-04-02 15:11:20 +0200

    [FIX] KahootUIState utilise QuestionWithSimpleReponse

--- android/app/src/main/java/fr/iut/sciencequest/ViewModels/UiStates/KahootUIState.kt
+++ android/app/src/main/java/fr/iut/sciencequest/ViewModels/UiStates/KahootUIState.kt
@@ -1,11 +1,13 @@
 package fr.iut.sciencequest.ViewModels.UiStates
 
+import fr.iut.sciencequest.model.dto.extensions.ToModel
 import fr.iut.sciencequest.model.dto.question.QuestionWithSimpleResponseDTO
 import fr.iut.sciencequest.model.dto.reponse.ReponseSimpleDTO
+import fr.iut.sciencequest.model.metier.question.QuestionWithSimpleReponse
 import fr.iut.sciencequest.stub.StubQuestionWithReponses
 
 data class KahootUIState (
-    val question: QuestionWithSimpleResponseDTO = StubQuestionWithReponses,
+    val question: QuestionWithSimpleReponse = StubQuestionWithReponses.ToModel(),
     val reponseChoisie: Boolean = false,
     // NOTE : Supposé en millisecondes
     val duréePartie: Long = 10_000,
