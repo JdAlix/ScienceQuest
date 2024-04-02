01cb8d35eb8dc446e480d59839384dff93e97925
Renaud BEURET <renaud.beuret@etu.uca.fr>
2024-04-02 15:06:48 +0200

    [ADD] QuestionWithSimpleReponse model

--- android/app/src/main/java/fr/iut/sciencequest/model/metier/question/QuestionWithSimpleReponse.kt
+++ android/app/src/main/java/fr/iut/sciencequest/model/metier/question/QuestionWithSimpleReponse.kt
@@ -1,10 +1,10 @@
 package fr.iut.sciencequest.model.metier.question
 
 import fr.iut.sciencequest.model.metier.reponse.Reponse
+import fr.iut.sciencequest.model.metier.reponse.ReponseSimple
 
-class Question(
+class QuestionWithSimpleReponse (
     val id: Int,
     val question: String,
-    val reponses: List<Reponse>
-) {}
-
+    val reponses: List<ReponseSimple>
+)
