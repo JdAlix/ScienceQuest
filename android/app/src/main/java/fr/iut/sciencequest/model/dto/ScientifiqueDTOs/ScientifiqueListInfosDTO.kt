96b9e10b86b5de1e4f9e76aa4cb9a68def6b1397
Alix JEUDI--LEMOINE <Alix.JEUDI--LEMOINE@etu.uca.fr>
2024-04-02 17:17:30 +0200

    Ajout code DTO

--- android/app/src/main/java/fr/iut/sciencequest/model/dto/ScientifiqueDTOs/ScientifiqueListInfosDTO.kt
+++ android/app/src/main/java/fr/iut/sciencequest/model/dto/ScientifiqueDTOs/ScientifiqueListInfosDTO.kt
@@ -1,4 +1,21 @@
 package fr.iut.sciencequest.model.dto.ScientifiqueDTOs
 
-class ScientifiqueListInfosDTO {
-}
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+open class ScientifiqueListInfosPageDTO(
+    @SerialName("totalElements")
+    val nbScientfiques: Int,
+
+    @SerialName("totalPages")
+    val nbPages: Int,
+
+    @SerialName("size")
+    val nbScientifiquesParPage: Int
+)
+
+@Serializable
+open class ScientifiqueListInfosDTO (
+    val page: ScientifiqueListInfosPageDTO
+)
