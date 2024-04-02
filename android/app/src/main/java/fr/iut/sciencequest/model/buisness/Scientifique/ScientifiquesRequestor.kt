4f5fbb321dfdf22338c4e6abc1363831ae33e6ca
Alix JEUDI--LEMOINE <Alix.JEUDI--LEMOINE@etu.uca.fr>
2024-04-02 17:15:52 +0200

    Ajout fetchScientifiquesInfos qui retourne le bloc page

--- android/app/src/main/java/fr/iut/sciencequest/model/buisness/Scientifique/ScientifiquesRequestor.kt
+++ android/app/src/main/java/fr/iut/sciencequest/model/buisness/Scientifique/ScientifiquesRequestor.kt
@@ -2,14 +2,8 @@ package fr.iut.sciencequest.model.buisness.Scientifique
 
 import android.util.Log
 import fr.iut.sciencequest.model.buisness.createRequestService
-import fr.iut.sciencequest.model.dto.ScientifiqueDTOs.ScientifiqueDTO
-import fr.iut.sciencequest.model.dto.ScientifiqueDTOs.ScientifiqueListDTO
 import kotlinx.coroutines.flow.flow
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.create
-import java.lang.IllegalArgumentException
 
 suspend fun fetchScientifiqueById(id: Int) = flow {
     val serviceClient = createRequestService().create<ScientifiqueRequestService>()
@@ -25,6 +19,16 @@ fun fetchScientifiques(index: Int) = flow {
     val serviceClient = createRequestService().create<ScientifiqueRequestService>()
     try {
         val response = serviceClient.getScientifiques(index)
+        emit(response)
+    } catch (e: Exception) {
+        Log.e("Requete API",e.message.toString())
+    }
+}
+
+fun fetchScientifiquesInfos() = flow {
+    val serviceClient = createRequestService().create<ScientifiqueRequestService>()
+    try {
+        val response = serviceClient.getScientifiquesListInfos().page
         emit(response)
     } catch (e: Exception) {
         Log.e("Requete API",e.message.toString())
