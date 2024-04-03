8727f38c108b36babaf3457e4f8ea99686338c09
gwenael.planchon <gwenael.planchon@etu.uca.fr>
2024-04-03 08:42:55 +0200

    enlever creer partie

--- science-quest/src/components/Titre.vue
+++ science-quest/src/components/Titre.vue
@@ -56,9 +56,9 @@ export default {
           <li class="nav-item">
             <router-link class="nav-link" to="/">Accueil</router-link>
           </li>
-          <li class="nav-item">
+          <!--li class="nav-item">
             <router-link class="nav-link" to="/partie">Créer une partie</router-link>
-          </li>
+          </li-->
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Jeux
